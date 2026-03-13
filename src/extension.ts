159a238671c882221470f81355f143c7df87f7db
Zofia Rogacka-Trojak <z.rogacka-trojak@student.tudelft.nl>
2026-03-13 21:38:12 +0100

    Onboarding

--- src/extension.ts
+++ src/extension.ts
@@ -2,8 +2,20 @@ import * as vscode from 'vscode';
 import { StatusBar } from './StatusBar'
 import { checkZombiePackages } from './zombiePackages';
 
+function handleOnboarding(context: vscode.ExtensionContext) {
+    const hasSeenOnboarding = context.globalState.get('flowState.hasSeenOnboarding');
+
+    if (!hasSeenOnboarding) {
+        const extensionId = context.extension.id;
+        const walkthroughId = `${extensionId}#flowState.welcome`;
+        
+        vscode.commands.executeCommand('workbench.action.openWalkthrough', walkthroughId, false);
+        context.globalState.update('flowState.hasSeenOnboarding', true);
+    }
+}
+
 export function activate(context: vscode.ExtensionContext) {
-    console.log('Congratulations, your extension "flow-state" is now active!');
+    handleOnboarding(context);
 
     const flowStateStatusBar = new StatusBar();
 
---
+++ walkthroughs/statusbar.md
@@ -0,0 +1,9 @@
+# Keep Your Flow Optimal
+
+Flow-State runs quietly in the background to make sure you aren't burning out. 
+
+Look at the bottom right of your VS Code window for the **Flow State** indicator. 
+
+Hovering over this indicator gives you a quick health-check of your current environment, including:
+- **Session details**
+- **Code Complexity** metrics to warn you if a file is getting too hard to read!
